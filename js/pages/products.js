da6ee4d13893c3afd698e42913568a146ea66273
Joris Neyens <neyens01@gmail.com>
2020-11-16 21:18:30 +0100

    products-page

--- js/pages/products.js
+++ js/pages/products.js
@@ -9,22 +9,25 @@ getProductsApi()
 export function makeProductsHtml(products) {
 
 
-
+    console.log(products)
     const cardContainer = document.querySelector("#card-container");
 
     let cardHtml= "";
 
     products.forEach(function(product) {
 
-        cardHtml += `<div class="card shadow-sm col-12 col-sm-5 offset-sm-0 col-md-5 col-lg-3" style="width: 18rem;">
+        cardHtml += `<a class="card shadow-sm col-12 col-sm-5 offset-sm-0 col-md-5 col-lg-3" href="product-page.html?id=${product.id}">
+                <div style="width: 18rem;">
                     <div class="card_image">
                         <img src="${baseUrl + product.image.url}" class="card-img-top" alt="${product.image.caption}">
                     </div>
                     <div class="card-body">
                         <h3 class="card-title text-center">${product.title}</h3>
                         <p class="card-text text-center">Starting at: <span class="price">$${product.price}</span></p>
+                            <i class="far fa-heart"></i>
+                            
                     </div>
-                </div>`
+                </div></a>`
 
 
     });
